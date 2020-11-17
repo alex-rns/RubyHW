26fde11e7cb97e0643abc08dfcb724b7a4354300
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-11-17 21:12:21 +0200

    Added fixed gem display_content v 1.0.0

--- HW2/Tamagotchi/display_content/lib/display_content.rb
+++ HW2/Tamagotchi/display_content/lib/display_content.rb
@@ -1,10 +1,11 @@
 require "display_content/version"
 require "erb"
+require "cgi/util"
 
 module DisplayContent
   class Error < StandardError; end
   def self.display_content(content, bypass_html = false, file_name = "index")
-    bypass_html ? content : content.gsub!(%r{<([^>]+?)([^>]*?)>(.*?)</\1>}, "")
+    bypass_html ? content : content = CGI.escapeHTML(content)
     output_file = File.expand_path("#{file_name}.html")
     template = %(<!DOCTYPE html>
   <html lang="en">
@@ -23,7 +24,6 @@ module DisplayContent
     result = renderer.result(binding)
     File.open(output_file, "w") do |f|
       f.write(result)
-      p "DisplayContent done!"
     end
   end
 end
--- HW2/Tamagotchi/display_content/lib/display_content/version.rb
+++ HW2/Tamagotchi/display_content/lib/display_content/version.rb
@@ -1,3 +1,3 @@
 module DisplayContent
-  VERSION = "0.1.0"
+  VERSION = "1.0.0"
 end
