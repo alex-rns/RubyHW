3b7f7ba058ad8b5223cd09931874dbaf6c361d7e
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-11-30 21:55:08 +0200

    The game "Guest Number" completely written.

--- HW4/app/lib/logic.rb
+++ HW4/app/lib/logic.rb
@@ -1,5 +1,4 @@
 module Logic
-
   def self.passed_time(request, emoji, text, minion_txt)
     Rack::Response.new do |response|
       response.set_cookie("age", request.cookies["age"].to_i + 1)
@@ -22,7 +21,7 @@ module Logic
       if request.cookies["hunger"].to_i >= 100
         response.set_cookie("text", "You did not feed your minion")
         response.set_cookie("minion_txt", "TATATA BALA TU!!")
-        self.die(request, response)
+        die(request, response)
       end
 
       if request.cookies["warning_txt"].start_with?("Attention! Minion can not sleep") && request.cookies["wc"].to_i < 70 && request.cookies["hunger"].to_i < 70
@@ -59,7 +58,7 @@ module Logic
       end
       if request.cookies["poo_poo"].to_i >= 5
         response.set_cookie("text", "It's too dirty around")
-        self.die(request, response)
+        die(request, response)
       end
 
       if request.cookies["mood"].to_i <= 20
@@ -85,23 +84,23 @@ module Logic
       response.set_cookie("mood", 100) if request.cookies["mood"].to_i > 100
       response.set_cookie("sleep", 100) if request.cookies["sleep"].to_i > 100
 
-      self.remove_attention(request, response, "Attention! Minion wants to sleep!", "sleep", 70 )
-      self.remove_attention(request, response, "Attention! Minion wants to sleep! Mood gets worse", "sleep", 90 )
-      self.remove_attention(request, response, "Attention! Feed your minion!", "hunger", 80 )
-      self.remove_attention(request, response, "Attention! Feed your minion before walk!", "hunger", 65 )
-      self.remove_attention(request, response, "Attention! Minion wants to go to the toilet!", "wc", 70 )
-      self.remove_attention(request, response, "Your minion went to the toilet in jeans", "wc", 90 )
-      self.remove_attention(request, response, "#{request.cookies["pet_name"]} lost one life", "age", 1 )
-      self.remove_attention(request, response, "Attention! It is too dirty", "poo_poo", 1 )
+      remove_attention(request, response, "Attention! Minion wants to sleep!", "sleep", 70)
+      remove_attention(request, response, "Attention! Minion wants to sleep! Mood gets worse", "sleep", 90)
+      remove_attention(request, response, "Attention! Feed your minion!", "hunger", 80)
+      remove_attention(request, response, "Attention! Feed your minion before walk!", "hunger", 65)
+      remove_attention(request, response, "Attention! Minion wants to go to the toilet!", "wc", 70)
+      remove_attention(request, response, "Your minion went to the toilet in jeans", "wc", 90)
+      remove_attention(request, response, "#{request.cookies["pet_name"]} lost one life", "age", 1)
+      remove_attention(request, response, "Attention! It is too dirty", "poo_poo", 1)
 
-      response.redirect('/start')
+      response.redirect("/start")
 
       if request.cookies["life"].to_i.zero?
         response.set_cookie("warning_txt", "Minion has no lives left")
         response.set_cookie("text", "You lost #{request.cookies["pet_name"]}")
         response.set_cookie("minion_txt", "POOPAYE...")
         response.set_cookie("emoji", "&#x2620;")
-        response.redirect('/game-over')
+        response.redirect("/game-over")
       end
     end
   end
@@ -116,7 +115,7 @@ module Logic
     Rack::Response.new do |response|
       response.set_cookie("hunger", request.cookies["hunger"] = -10)
     end
-    self.passed_time(request, emoji, text, minion_txt)
+    passed_time(request, emoji, text, minion_txt)
   end
 
   def self.go_sleep(request, emoji, text, minion_txt)
@@ -126,13 +125,13 @@ module Logic
         response.set_cookie("text", text)
         response.set_cookie("emoji", "&#x1F914;")
         response.set_cookie("warning_txt", "Attention! Minion can not sleep. Something bothers him")
-        response.redirect('/start')
+        response.redirect("/start")
       end
     else
       Rack::Response.new do |response|
         response.set_cookie("sleep", request.cookies["sleep"] = -5)
       end
-      self.passed_time(request, emoji, text, minion_txt)
+      passed_time(request, emoji, text, minion_txt)
     end
   end
 
@@ -140,14 +139,14 @@ module Logic
     Rack::Response.new do |response|
       response.set_cookie("wc", request.cookies["wc"] = -10)
     end
-    self.passed_time(request, emoji, text, minion_txt)
+    passed_time(request, emoji, text, minion_txt)
   end
 
   def self.go_walk(request, emoji, text, minion_txt)
     if request.cookies["hunger"].to_i >= 70
       Rack::Response.new do |response|
         response.set_cookie("warning_txt", "Attention! Feed your minion before walk!")
-        response.redirect('/start')
+        response.redirect("/start")
       end
     else
       Rack::Response.new do |response|
@@ -158,7 +157,7 @@ module Logic
         response.set_cookie("minion_txt", minion_txt)
         response.set_cookie("emoji", emoji)
       end
-      self.passed_time(request, emoji, text, minion_txt)
+      passed_time(request, emoji, text, minion_txt)
     end
   end
 
@@ -166,13 +165,13 @@ module Logic
     if request.cookies["poo_poo"].to_i.zero?
       Rack::Response.new do |response|
         response.set_cookie("text", "There is nothing to clean up")
-        response.redirect('/start')
+        response.redirect("/start")
       end
     else
       Rack::Response.new do |response|
         response.set_cookie("poo_poo", request.cookies["poo_poo"] = request.cookies["poo_poo"].to_i - 1)
       end
-      self.passed_time(request, emoji, text, minion_txt)
+      passed_time(request, emoji, text, minion_txt)
     end
   end
 
@@ -184,7 +183,7 @@ module Logic
         response.set_cookie("study", request.cookies["study"] = request.cookies["study"].to_i + 2)
       end
     end
-    self.passed_time(request, emoji, text, minion_txt)
+    passed_time(request, emoji, text, minion_txt)
   end
 
   def self.super_skill(request, emoji, text, minion_txt)
@@ -193,7 +192,7 @@ module Logic
         response.set_cookie("text", "Sorry, but you minion has not evolved yet")
         response.set_cookie("minion_txt", "UNDERWEAR…")
         response.set_cookie("emoji", "&#x1F97A;")
-        response.redirect('/start')
+        response.redirect("/start")
       end
     else
       Rack::Response.new do |response|
@@ -204,32 +203,55 @@ module Logic
         response.set_cookie("text", text)
         response.set_cookie("minion_txt", minion_txt)
         response.set_cookie("emoji", emoji)
-        response.redirect('/start')
+        response.redirect("/start")
       end
     end
   end
 
-  def self.guess_number(request, emoji, text, minion_txt)
+  def self.guess_number_init(request, emoji, text, minion_txt)
     Rack::Response.new do |response|
       response.set_cookie("text", text)
       response.set_cookie("minion_txt", minion_txt)
       response.set_cookie("emoji", emoji)
-      response.set_cookie("warning_txt", request.params["guess_number"])
-      response.redirect('/guess-number')
+      response.set_cookie("guess_number", rand(1..100))
+      response.set_cookie("warning_txt", request.params["guess_number_value"])
+      response.redirect("/guess-number")
+    end
+  end
+
+  def self.guess_number_game(request)
+    guess_number_value = request.params["guess_number_value"].to_i
+    number = request.cookies["guess_number"].to_i
+    Rack::Response.new do |response|
+      if guess_number_value > number
+        response.set_cookie("text", "Your number is greater.")
+        response.set_cookie("minion_txt", "DUL!")
+        response.set_cookie("emoji", "&#x1F609;")
+      elsif guess_number_value < number
+        response.set_cookie("text", "Your number is less.")
+        response.set_cookie("minion_txt", "SAE!")
+        response.set_cookie("emoji", "&#x1F643;")
+      else
+        response.set_cookie("text", "Right! Guess number - #{guess_number_value}")
+        response.set_cookie("minion_txt", "TULALILOO TI AMO!")
+        response.set_cookie("emoji", "&#x1F61D;")
+        response.set_cookie("mood", request.cookies["mood"] = 100)
+      end
+      response.redirect("/guess-number")
     end
   end
 
   def self.die(request, response)
-      response.set_cookie("life", request.cookies["life"].to_i - 1)
-      response.set_cookie("emoji", "&#x1F631;")
-      response.set_cookie("age", 0)
-      response.set_cookie("hunger", 0)
-      response.set_cookie("mood", 100)
-      response.set_cookie("wc", 0)
-      response.set_cookie("study", 0)
-      response.set_cookie("sleep", 0)
-      response.set_cookie("pet_name", request.cookies["pet_name"].delete_prefix("Super")) if request.cookies["evolution"].to_i == 1
-      response.set_cookie("evolution", 0)
-      response.set_cookie("warning_txt", "#{request.cookies["pet_name"]} lost one life, #{request.cookies["life"].to_i - 1} left")
+    response.set_cookie("life", request.cookies["life"].to_i - 1)
+    response.set_cookie("emoji", "&#x1F631;")
+    response.set_cookie("age", 0)
+    response.set_cookie("hunger", 0)
+    response.set_cookie("mood", 100)
+    response.set_cookie("wc", 0)
+    response.set_cookie("study", 0)
+    response.set_cookie("sleep", 0)
+    response.set_cookie("pet_name", request.cookies["pet_name"].delete_prefix("Super")) if request.cookies["evolution"].to_i == 1
+    response.set_cookie("evolution", 0)
+    response.set_cookie("warning_txt", "#{request.cookies["pet_name"]} lost one life, #{request.cookies["life"].to_i - 1} left")
   end
 end
--- HW4/app/pet.rb
+++ HW4/app/pet.rb
@@ -1,6 +1,6 @@
 require "erb"
-require './app/lib/logic'
-require 'rack'
+require "./app/lib/logic"
+require "rack"
 
 class Pet
   include Logic
@@ -21,7 +21,7 @@ class Pet
     @poo_poo = 0
     @evolution = 0
     @emoji = "&#x1F92A;"
-    @text = "Minion #{@request.params['pet_name']} came to you!"
+    @text = "Minion #{@request.params["pet_name"]} came to you!"
     @minion_txt = "BELLO!"
   end
 
@@ -31,80 +31,84 @@ class Pet
       Rack::Response.new(render("welcome.html.erb"))
     when "/welcome"
       Rack::Response.new do |response|
-        response.set_cookie('pet_name', @request.params['pet_name'].capitalize)
-        response.set_cookie('life', @life)
-        response.set_cookie('age', @age)
-        response.set_cookie('mood', @mood)
-        response.set_cookie('hunger', @hunger)
-        response.set_cookie('wc', @wc)
-        response.set_cookie('sleep', @sleep)
-        response.set_cookie('study', @study)
-        response.set_cookie('poo_poo', @poo_poo)
-        response.set_cookie('evolution', @evolution)
-        response.set_cookie('emoji', @emoji)
-        response.set_cookie('text', @text)
-        response.set_cookie('minion_txt', @minion_txt)
-        response.set_cookie('warning_txt', "")
-        response.redirect('/start')
+        response.set_cookie("pet_name", @request.params["pet_name"].capitalize)
+        response.set_cookie("life", @life)
+        response.set_cookie("age", @age)
+        response.set_cookie("mood", @mood)
+        response.set_cookie("hunger", @hunger)
+        response.set_cookie("wc", @wc)
+        response.set_cookie("sleep", @sleep)
+        response.set_cookie("study", @study)
+        response.set_cookie("poo_poo", @poo_poo)
+        response.set_cookie("evolution", @evolution)
+        response.set_cookie("emoji", @emoji)
+        response.set_cookie("text", @text)
+        response.set_cookie("minion_txt", @minion_txt)
+        response.set_cookie("warning_txt", "")
+        response.redirect("/start")
       end
     when "/start"
       Rack::Response.new(render("index.html.erb"))
-    when "/guess-number"
-      Rack::Response.new(render("game-guess-number.html.erb"))
     when "/game-over"
       Rack::Response.new(render("game-over.html.erb"))
-    when '/action_of_button'
+    when "/action_of_button"
       return Logic.feed(
         @request,
         "&#x1F924;",
         "You give #{@request.cookies["pet_name"]} his favorite food",
-        "BA-NA-NA!") if @request.params['feed']
+        "BA-NA-NA!"
+      ) if @request.params["feed"]
       return Logic.go_sleep(
         @request,
         "&#x1F634;",
         "You put #{@request.cookies["pet_name"]} to bed",
-        "MUAK MUAK MUAK!") if @request.params['sleep']
+        "MUAK MUAK MUAK!"
+      ) if @request.params["sleep"]
       return Logic.go_wc(
         @request,
         "&#x1F92B;",
         "Minion goes to the toilet",
-        "PWEDE NA?") if @request.params['wc']
+        "PWEDE NA?"
+      ) if @request.params["wc"]
       return Logic.go_walk(
         @request,
         "&#x1F601;",
         "You go for a walk with #{@request.cookies["pet_name"]}",
-        "KAMPAI!") if @request.params['walk']
+        "KAMPAI!"
+      ) if @request.params["walk"]
       return Logic.clean_up(
         @request,
         "&#x1F644;",
         "We cleaned up around",
-        "GELATO!") if @request.params['clean']
+        "GELATO!"
+      ) if @request.params["clean"]
       return Logic.teach(
         @request,
         "&#x1F913;",
         "You minion is learning Ruby",
-        "WOW! BOBMA!") if @request.params['teach']
+        "WOW! BOBMA!"
+      ) if @request.params["teach"]
       return Logic.super_skill(
         @request,
         "&#x1F92F;",
-      "All parameters are restored!",
-        "BEE DO BEE DO BEE DO!") if @request.params['super']
-      return Rack::Response.new(render("mini-game-choose.html.erb")) if @request.params['play']
-    when '/mini-game'
-      return Rack::Response.new(render("game-guess-number.html.erb")) if @request.params['guess']
-      return Rack::Response.new(render("index.html.erb")) if @request.params['exit']
-    when '/guess'
-      # Rack::Response.new do |response|
-      #   response.set_cookie('guess_number', @request.params['guess_number'])
-      #   response.redirect('/guess-number')
-      # end
-      return Logic.guess_number(
+        "All parameters are restored!",
+        "BEE DO BEE DO BEE DO!"
+      ) if @request.params["super"]
+      return Rack::Response.new(render("mini-game-choose.html.erb")) if @request.params["play"]
+    when "/mini-game"
+      return Logic.guess_number_init(
         @request,
         "&#x1F9D0;",
         "The minion thought of a number from 1 to 100, guess the number",
-        "HANA!") if @request.params['guess_number']
+        "HANA!"
+      ) if @request.params["guess"]
+      return Rack::Response.new(render("index.html.erb")) if @request.params["exit"]
+    when "/guess-number"
+      Rack::Response.new(render("game-guess-number.html.erb"))
+    when "/guess-start"
+      return Logic.guess_number_game(@request) if @request.params["guess_number_value"]
     else
-      Rack::Response.new('Not Found', 404)
+      Rack::Response.new("Not Found", 404)
     end
   end
 
@@ -114,6 +118,6 @@ class Pet
   end
 
   def get(attr)
-    @request.cookies["#{attr}"]
+    @request.cookies[attr]
   end
 end
